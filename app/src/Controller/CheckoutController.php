b7a0abd4ae0db2416f269166ca2d8479986b4917
Vladyslav Drybas <ukdvvw@gmail.com>
2024-08-15 12:39:11 +0200

    add buttons template

---
+++ app/src/Controller/CheckoutController.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(name: "app")]
+class CheckoutController extends AbstractController
+{
+    #[Route("/checkout", name: "_checkout", methods: ["GET", "OPTIONS", "HEAD"])]
+    public function index(): Response
+    {
+        return $this->render(
+            'checkout/index.html.twig',
+            [
+                'meta' => [
+                    'title' => 'Prototyper|Checkout',
+                ],
+            ]
+        );
+    }
+}
