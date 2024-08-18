45723892fca84223fca4cc7cab2ace047eb98f6e
Vladyslav Drybas <ukdvvw@gmail.com>
2024-08-18 12:47:16 +0200

    add logic for replace page with blocks

---
+++ app/src/Controller/CheckPageReplaceController.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(name: "app")]
+class CheckPageReplaceController extends AbstractController
+{
+    #[Route("/replace", name: "_replace", methods: ["GET", "OPTIONS", "HEAD"])]
+    public function index(): Response
+    {
+        return $this->render(
+            'replace/index.html.twig',
+        );
+    }
+}
