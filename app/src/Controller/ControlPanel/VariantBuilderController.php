47665b9b58aba49c80d1b548e9c2a10f3a91ce77
Vladyslav Drybas <ukdvvw@gmail.com>
2024-08-21 22:52:04 +0200

    add variant builder preview stub and first parts

--- app/src/Controller/ControlPanel/VariantBuilderController.php
+++ app/src/Controller/ControlPanel/VariantBuilderController.php
@@ -5,11 +5,7 @@ namespace App\Controller\ControlPanel;
 
 use App\Constants\RouteRequirements;
 use App\Entity\Variant;
-use App\Form\CommandCenter\Variant\VariantAddFormType;
-use App\Form\CommandCenter\Variant\VariantEditFormType;
-use App\Repository\VariantRepository;
-use Exception;
-use InvalidArgumentException;
+use App\Form\CommandCenter\VariantBuilder\VariantBuilderFormType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -29,12 +25,33 @@ class VariantBuilderController extends AbstractControlPanelController
         methods: ['GET', 'POST']
     )]
     public function show(
+        Request $request,
         Variant $variant
     ): Response {
+
+        $builderForm = $this->createForm(VariantBuilderFormType::class, [
+
+        ]);
+
+        $builderForm->handleRequest($request);
+
+        $data = $this->getVariantMeta($request, $variant);
+
+        if ($builderForm->isSubmitted() && $builderForm->isValid()) {
+            dump('Submitted');
+            dump($builderForm->getData());
+            $request->getSession()->set(
+                'variantBuilderData',
+                $this->builderData($variant)
+            );
+        }
+
         return $this->render(
             'control-panel/variant/builder/index.html.twig',
             [
                 'variant' => $variant,
+                'builderForm' => $builderForm,
+                'data' => $data,
             ]
         );
     }
@@ -45,13 +62,66 @@ class VariantBuilderController extends AbstractControlPanelController
         methods: ['GET']
     )]
     public function preview(
+        Request $request,
         Variant $variant
     ): Response {
+        $data = $this->getVariantMeta($request, $variant);
+
+        dump($data);
+
         return $this->render(
-            'control-panel/variant/builder/index.html.twig',
+            '_parts/base/base.html.twig',
             [
                 'variant' => $variant,
+                'data' => $data,
             ]
         );
+    }
+
+    protected function getVariantMeta(
+        Request $request,
+        Variant $variant
+    ): array {
+        $data = $request->getSession()->get('variantBuilderData');
+
+        if (null === $data) {
+            $data = $this->builderData($variant);
+        }
+
+        return $data;
+    }
+
+    protected function builderData(
+        Variant $variant
+    ): array {
+        return [
+            'variant_id' => $variant->getRawId(),
+            'project_id' => $variant->getProject()->getRawId(),
+            'parts' => [
+                'header' => [
+                    'isActive' => true,
+                    'position' => 0,
+                    'type' => 'header',
+                    'template' => 'testLaunch',
+                    'data' => [
+                        'brand' => [
+                            'logo' => 'brand-ai-cv.svg',
+                            'text' => 'AI/CV',
+                        ],
+                        'navigation' => [
+                            'Home' => '#home',
+                            'Features' => '#features',
+                            'Testimonials' => '#testimonials',
+                            'Pricing' => '#pricing',
+                            'Contact' => '#contact',
+                        ],
+                        'callToActionButton' => [
+                            'text' => 'Get Started',
+                            'link' => '#pricing',
+                        ]
+                    ],
+                ],
+            ],
+        ];
     }
 }
